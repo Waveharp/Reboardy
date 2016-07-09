87f683da7cf713e3becd739b739d0845b94ec1fa
Josh Stricklin <jostricklin@gmail.com>
2016-07-09 13:50:59 -0500

    rewrite existing components to use rebass

--- client/components/Game.js
+++ client/components/Game.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import { Card, CardImage, Heading, Text } from 'rebass';
+
 const Game = React.createClass({
 	render() {
 		const { game, i, comments } = this.props;
 		return (
-			<figure className="grid-figure">
-				<div className="grid-game-wrap">
-					<Link to={`/view/${game.objectid}`}>
-						<img className="grid-image" src={game.image} />
-						<h3>{game.name}</h3>
-					</Link>
-				</div>
+			<figure>
 
-				<figcaption>
-					<div className="control-buttons">
-						<button className="likes" onClick={this.props.increment.bind(null, i)}>&hearts; {game.likes}</button>
-						<Link to={`view/${game.objectid}`}>
-							<span className="comment-count">
-								{comments[game.objectid] ? comments[game.objectid].length : 0}
-							</span>
-						</Link>
-					</div>
-				</figcaption>
+				<Card
+					rounded
+					width={256}
+				>
+					<Link to={`/view/${game.objectid}`}>
+						<CardImage src={game.image} />
+						<Heading
+							level={2}
+							size={3}
+						>
+							{game.name}
+						</Heading>
+					</Link>	
+					<figcaption>
+						<div className="control-buttons">
+							<button className="likes" onClick={this.props.increment.bind(null, i)}>&hearts; {game.likes}</button>
+							<Link to={`view/${game.objectid}`}>
+								<span className="comment-count">
+									{comments[game.objectid] ? comments[game.objectid].length : 0}
+								</span>
+							</Link>
+						</div>
+					</figcaption>
+				</Card>
+
 
 			</figure>
 		)
@@ -30,3 +41,10 @@ const Game = React.createClass({
 });
 
 export default Game;
+				
+// <div className="grid-game-wrap">
+// 	<Link to={`/view/${game.objectid}`}>
+// 		<img className="grid-image" src={game.image} />
+// 		<h3>{game.name}</h3>
+// 	</Link>
+// </div>
