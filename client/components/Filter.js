ecb3f849bba4524b50d9c4f4104a8a508777f466
Josh Stricklin <jostricklin@gmail.com>
2016-07-11 00:19:40 -0500

    create filter component

---
+++ client/components/Filter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Filter = React.createClass({
+	render() {
+		return (
+			<div className="filter">
+				<p>I'm the filter.</p>
+			</div>
+		)
+	}
+});
+
+export default Filter;
