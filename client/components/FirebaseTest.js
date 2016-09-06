e18b1ab4107fa7a6455323a385bc36dd7f91a0d9
Josh Stricklin <jostricklin@gmail.com>
2016-09-06 01:53:01 -0500

    add lodash

--- client/components/FirebaseTest.js
+++ client/components/FirebaseTest.js
@@ -4,7 +4,8 @@ const FirebaseTest = React.createClass({
 	render() {
 		return (
 			<div>
-				<h1>I'm the firebase test page.</h1>
+				<h1>List of games from my Firebase DB:</h1>
+				<p>is in the console.</p>
 			</div>
 		)
 	}
